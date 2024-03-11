b95383c6dbc46a02c82d102a9d4432df762e52fc
Abdurrahman Hac Said <92548247+ABDURRAHMAN4498@users.noreply.github.com>
2024-03-11 17:17:36 +0300

    first commit

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EF_Test.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Test
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Connections.sqlConStr);
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Connections.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Test
+{
+    public class Connections
+    {
+        public const string sqlConStr = """
+            server = DESKTOP-90P1V0P;
+            Database= dbEfTest;
+            User Id = DESKTOP-90P1V0P\User;
+            Password=;
+            TrustServerCertificate=true;
+            """;
+    }
+}
